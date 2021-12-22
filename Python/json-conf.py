99f089dc8cf5e221f55086de2b40274755c656c3
Alex Collier <alex.collier@version1.com>
2021-12-22 10:06:13 +0000

    Monolithic conf file option & HAProxy template

--- Python/json-conf.py
+++ Python/json-conf.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 # Build config files based on a JSON input file
+# Alex Collier, Version 1, December 2021
 
 import json
 from jinja2 import Template, Environment, FileSystemLoader
+import os
 
-conffn = "conf.json"
-conftype = "vhost"
+conffn = "conf.json" # JSON data file
+conftype = "haproxy" # what sort of output to create. There must be a matching '.jinja' template file
 tplfile = conftype + ".jinja"
+templatevaluelist = [] # Array of template values for monolithic files. 
+generatedby = os.path.basename(__file__) # basename $0
+multifileset = {"vhost"} # Config types in this set are treated as multi-file items
+if conftype in multifileset:
+    multifile = True # If True, create a set of files using a template with simple values
+else:
+    multifile = False # If False, create one monolithic file using a template with looping over an array of values
 
 # File handling for the template file
 file_loader = FileSystemLoader('.')
@@ -31,7 +40,17 @@ with open(conffn) as json_fh:
                 'upstream' : appdata['upstream'],
                 'external' : appdata['external']
             }
+            
             # Render the template into the confout variable and write it out to the config file
-            confout = template.render(templatevalues=templatevalues)
-            with open(conffile, 'w') as conf_fh:
-                print(confout, file=conf_fh)
+            if multifile:
+                confout = template.render(templatevalues=templatevalues, generatedby=generatedby)
+                with open(conffile, 'w') as conf_fh:
+                    print(confout, file=conf_fh)
+            else:
+                templatevaluelist.append(templatevalues)
+
+if not multifile:
+    confout = template.render(templatevalues=templatevaluelist, generatedby=generatedby)
+    conffile = conftype + ".conf"
+    with open(conffile, 'w') as conf_fh:
+        print(confout, file=conf_fh)
